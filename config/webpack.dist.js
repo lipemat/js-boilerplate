2f7fd7135a4f3b021d4e790346d9f17f974d3518
lipemat <mat@matlipe.com>
2019-02-19 15:08:47 -0500

    support js or jsx files just in case

--- config/webpack.dist.js
+++ config/webpack.dist.js
@@ -39,7 +39,7 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.js$/,
+				test: /\.jsx?$/,
 				loader: 'babel-loader',
 				include: path.resolve( config.workingDirectory, 'src' ),
 				exclude: /node_modules/,
