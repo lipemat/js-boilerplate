dd08de98c1ffd3c7ccf1b46ea84edf6baa52162b
lipemat <mat@matlipe.com>
2019-02-19 12:42:09 -0500

    remove unnecessary verbose path for jest

--- templates/jest.config.js
+++ templates/jest.config.js
@@ -6,8 +6,7 @@
  *
  * @notice may become available later https://github.com/facebook/jest/pull/7696
  *
- * @type {{transform, testURL, moduleNameMapper, setupFilesAfterEnv, verbose}|*}
  */
-let config = require( './node_modules/lipemat-js-boilerplate/tests/jest.config' );
+let config = require( 'lipemat-js-boilerplate/tests/jest.config' );
 
 module.exports = config;
