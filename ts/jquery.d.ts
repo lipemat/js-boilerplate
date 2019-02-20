450eb622c5f70147ea5367611c6eb99521b0f8ec
lipemat <mat@matlipe.com>
2019-02-20 08:10:49 -0500

    Add jQuery ts for use

---
+++ ts/jquery.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Must have the @types/jquery typescript installed
+ * either via PHPStorm -> Javascript -> Libraries
+ * or `npm i @types/jquery`
+ *
+ * @link https://stackoverflow.com/a/54639561/3944788
+ *
+ * @since 2019-02-11
+ *
+ */
+interface jQuery<TElement = HTMLElement> extends JQuery {}
