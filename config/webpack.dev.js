ba77b4fbb506d48bf7d35865d559ac7d4e0c55ee
lipemat <mat@matlipe.com>
2019-08-30 10:37:21 -0400

    Use M emoji and short class names to distingush generated classes

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -74,7 +74,7 @@ module.exports = {
 						loader: 'css-loader',
 						options: {
 							importLoaders: 1,
-							localIdentName: '[path]-[name]__[local]',
+							localIdentName: 'Ⓜ[name]__[local]',
 							modules: true,
 							sourceMap: true,
 							url: false
