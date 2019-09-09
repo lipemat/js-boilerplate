b62f38905ad6c36e214f67fd9a0f96b24172385d
lipemat <mat@matlipe.com>
2019-09-09 14:32:48 -0400

    Generate .revision file on dist

--- scripts/dist.js
+++ scripts/dist.js
@@ -1,8 +1,29 @@
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
 
+const fs = require( 'fs' );
+const path = require( 'path' );
 const webpack = require( 'webpack' );
 const webpackConfig = require( '../helpers/config' ).getConfig( 'webpack.dist.js' );
+const packageConfig = require( '../helpers/package-config' );
+
+/**
+ * Bump the .revision file to the latest
+ * Useful when PWA is active because Chrome will reload when developing via workspaces
+ * but reloading the page will get the old "Application" cached resources unless we
+ * bump the revision.
+ * All helps with sha integrity issues on developing locally.
+ */
+function updateRevisionFile() {
+	if ( packageConfig.root ) {
+		fs.writeFile( path.resolve( packageConfig.root, '.revision' ), Date.now(), err => {
+			if ( err ) {
+				throw err;
+			}
+			console.log( '\n', 'Generated .revision file', '\n' );
+		} );
+	}
+}
 
 
 async function build( config ) {
@@ -11,8 +32,14 @@ async function build( config ) {
 
 	// Run the compiler.
 	compiler.run( ( err, stats ) => {
+		if ( err ) {
+			throw err;
+		}
 		console.log( stats.toString() );
+		updateRevisionFile();
 	} );
-
 }
+
+
 build( webpackConfig );
+
