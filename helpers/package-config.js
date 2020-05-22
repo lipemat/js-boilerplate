64e85c0d20c8379749e2b1dab87229063fa1da9b
lipemat <mat@matlipe.com>
2020-05-22 15:15:32 -0600

    Add defaults for theme_path and root which point to current directory
    Makes the JS app live in the top level directory by default.

--- helpers/package-config.js
+++ helpers/package-config.js
@@ -7,9 +7,9 @@ const workingDirectory = fs.realpathSync( process.cwd() );
 
 let packageConfig = require( path.resolve( workingDirectory, 'package.json' ) );
 packageConfig.workingDirectory = workingDirectory;
-packageConfig.theme_path = packageConfig.theme_path || '';
+packageConfig.theme_path = packageConfig.theme_path || '/';
 packageConfig.url = packageConfig.url || 'https://localhost';
-packageConfig.root = packageConfig.root || '';
+packageConfig.root = packageConfig.root || './';
 packageConfig.regenerate_revision = packageConfig.regenerate_revision || false;
 
 try {
