8dcf292368301ff6e908e59d2d7a0e004f6f7072
lipemat <mat@matlipe.com>
2020-06-01 08:20:39 -0600

    Add dependencies default to package-config.js

--- helpers/package-config.js
+++ helpers/package-config.js
@@ -6,6 +6,7 @@ const fs = require( 'fs' );
 const workingDirectory = fs.realpathSync( process.cwd() );
 
 let packageConfig = require( path.resolve( workingDirectory, 'package.json' ) );
+packageConfig.dependencies = packageConfig.dependencies || {};
 packageConfig.workingDirectory = workingDirectory;
 packageConfig.theme_path = packageConfig.theme_path || '/';
 packageConfig.url = packageConfig.url || 'https://localhost';
@@ -13,7 +14,7 @@ packageConfig.root = packageConfig.root || './';
 packageConfig.regenerate_revision = packageConfig.regenerate_revision || false;
 
 try {
-	let localConfig = require( path.resolve( workingDirectory, './local-config.json' ) );
+	const localConfig = require( path.resolve( workingDirectory, './local-config.json' ) );
 	packageConfig = { ...packageConfig, ...localConfig }
 } catch ( e ) {
 }
