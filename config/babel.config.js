41f95c279f7d4ddf9a249b444a35d63b50179bcb
lipemat <mat@matlipe.com>
2021-02-02 14:32:37 -0700

    Allow the browserslist config to be honored in babel
    If a config is specified we don't specify a target which lets Babel use the browserslist config.
    
    If a config is not specified, we pass "default" to Babel which allows using the default for browserslist.
    
    @notice Will change in Babel 8.
    @link https://babeljs.io/docs/en/babel-preset-env#targets

--- config/babel.config.js
+++ config/babel.config.js
@@ -1,14 +1,27 @@
+const browserslist = require( 'browserslist' );
+
+const presetEnv = {
+	corejs: {
+		version: '3.8',
+	},
+	shippedProposals: true,
+	useBuiltIns: 'usage',
+};
+/**
+ * Requirement of Babel preset when no browserlist config is specified.
+ *
+ * @notice Will change in Babel 8.
+ *
+ * @link https://babeljs.io/docs/en/babel-preset-env#targets
+ */
+if ( browserslist( browserslist.defaults ) === browserslist() ) {
+	presetEnv.targets = 'defaults';
+}
+
 module.exports = {
 	cacheDirectory: true,
 	presets: [
-		[ '@babel/preset-env', {
-			corejs: {
-				version: '3.8',
-			},
-			shippedProposals: true,
-			targets: 'defaults',
-			useBuiltIns: 'usage',
-		} ],
+		[ '@babel/preset-env', presetEnv ],
 		'@babel/preset-react',
 		'@babel/preset-typescript',
 	],
