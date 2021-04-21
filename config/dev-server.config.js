3e9c214e2c31630b44a97431cefe488a44b7d015
lipemat <mat@matlipe.com>
2021-04-20 21:36:47 -0600

    Support loading https certificates from package.json
    Yarn PNP cannot generate default certificates for webpack because .zip
    files are read-only and will receive the following error
    
    `Error: EROFS: read-only filesystem, open`
    
    If not URL is specified, this will now fallback to `http://localhost`
    instead fo `https://localhost` to prevent this error.
    
    Certificates may now be specified within package.json and will work the
    same as original `dev-server.config.js` overrides.
    
    ```json
      "certificates" : {
        "cert": "D:/xampp/apache/certs/crmtest.matlipe.com-crt.pem",
        "key": "D:/xampp/apache/certs/crmtest.matlipe.com-key.pem"
      }
    ```

--- config/dev-server.config.js
+++ config/dev-server.config.js
@@ -1,14 +1,24 @@
+const fs = require( 'fs' );
 const configHelper = require('../helpers/config' );
 const webpackConfig = configHelper.getConfig('webpack.dev.js');
 const packageConfig = require( '../helpers/package-config' );
 
 const url = new URL( packageConfig.url );
 
+let https = url.protocol === 'https:';
+// Load local certificates for https during development.
+if ( 'object' === typeof( packageConfig.certificates ) ) {
+	https = {
+		cert: fs.readFileSync( packageConfig.certificates.cert ),
+		key: fs.readFileSync( packageConfig.certificates.key ),
+	};
+}
+
 module.exports = {
 	disableHostCheck: true,
 	host: '0.0.0.0', // Allow connections from outside localhost to support mobile debugging
 	hot: true,
-	https: url.protocol === 'https:',
+	https,
 	historyApiFallback: true,
 	headers: {
 		'Access-Control-Allow-Origin': '*',
