33b93eff79dace4475d36e2553a7d09142412e9d
lipemat <mat@matlipe.com>
2021-09-19 22:23:08 -0400

    Make tests work with a custom workingDirectory
    Pass the config file as a command line argument to the jest command.

--- scripts/test.js
+++ scripts/test.js
@@ -5,4 +5,8 @@
  *
  * @example `lipemat-js-boilerplate test`
  */
+
+const packageConfig = require( '../helpers/package-config' );
+process.argv.push( '--config', packageConfig.workingDirectory + '/jest.config.js' );
+
 require( 'jest-cli/bin/jest' );
