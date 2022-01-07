61f3fe157ecfd8e5808c3bb25f36f2503f65fad8
lipemat <mat@matlipe.com>
2022-01-07 13:26:34 -0500

    Migrate from Webpack dev server V3 to V4
    https://github.com/webpack/webpack-dev-server/blob/master/migration-v4.md

--- config/dev-server.config.js
+++ config/dev-server.config.js
@@ -1,13 +1,13 @@
 const fs = require( 'fs' );
-const configHelper = require('../helpers/config' );
-const webpackConfig = configHelper.getConfig('webpack.dev.js');
+const configHelper = require( '../helpers/config' );
+const webpackConfig = configHelper.getConfig( 'webpack.dev.js' );
 const packageConfig = require( '../helpers/package-config' );
 
 const url = new URL( packageConfig.url );
 
-let https = url.protocol === 'https:';
+let https = 'https:' === url.protocol;
 // Load local certificates for https during development.
-if ( 'object' === typeof( packageConfig.certificates ) ) {
+if ( 'object' === typeof ( packageConfig.certificates ) ) {
 	https = {
 		cert: fs.readFileSync( packageConfig.certificates.cert ),
 		key: fs.readFileSync( packageConfig.certificates.key ),
@@ -15,7 +15,14 @@ if ( 'object' === typeof( packageConfig.certificates ) ) {
 }
 
 module.exports = {
-	disableHostCheck: true,
+	allowedHosts: 'all',
+	client: {
+		webSocketURL: {
+			hostname: url.hostname,
+			port: 3000,
+		},
+	},
+	compress: false,
 	host: '0.0.0.0', // Allow connections from outside localhost to support mobile debugging
 	hot: true,
 	https,
@@ -23,10 +30,9 @@ module.exports = {
 	headers: {
 		'Access-Control-Allow-Origin': '*',
 		'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-		'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+		'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
 	},
 	port: 3000,
-	public: url.hostname + ':3000',
 	publicPath: webpackConfig.output.publicPath,
-	stats: webpackConfig.stats
+	stats: webpackConfig.stats,
 };
--- scripts/start.js
+++ scripts/start.js
@@ -3,19 +3,22 @@ process.env.NODE_ENV = 'development';
 
 const webpack = require( 'webpack' );
 const WebpackDevServer = require( 'webpack-dev-server' );
-const configHelper = require('../helpers/config' );
-const webpackConfig = configHelper.getConfig('webpack.dev.js');
-const devServerConfig = configHelper.getConfig('dev-server.config.js');
+const configHelper = require( '../helpers/config' );
+const webpackConfig = configHelper.getConfig( 'webpack.dev.js' );
+const devServerConfig = configHelper.getConfig( 'dev-server.config.js' );
 
-const server = new WebpackDevServer( webpack( webpackConfig ), devServerConfig );
+const server = new WebpackDevServer( devServerConfig, webpack( webpackConfig ) );
 
-server.listen( devServerConfig.port, devServerConfig.host, function( err, result ) {
-	if ( err ) {
+( async() => {
+	try {
+		await server.start();
+	} catch ( err ) {
 		return console.log( err );
 	}
+
 	if ( devServerConfig.https ) {
 		console.log( `Listening at https://${devServerConfig.host}:${devServerConfig.port}/` );
 	} else {
 		console.log( `Listening at http://${devServerConfig.host}:${devServerConfig.port}/` );
 	}
-} );
+} )();
