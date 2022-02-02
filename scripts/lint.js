7b18fce9d3583adc3e8529ab40dd73673e0da28e
lipemat <mat@matlipe.com>
2022-02-01 21:42:40 -0500

    Convert `lint` script to ESLint's public API
    @link https://eslint.org/docs/developer-guide/nodejs-api

--- scripts/lint.js
+++ scripts/lint.js
@@ -1,4 +1,32 @@
-const eslint = require( 'eslint/lib/cli' );
+const {ESLint} = require( 'eslint' );
 const packageConfig = require( '../helpers/package-config' );
 
-eslint.execute( [ '', '', packageConfig.workingDirectory + '/src', '--fix', '--ext', '.tsx,.ts,.js,.jsx' ] );
+/**
+ * Use the public API to run the eslint commands.
+ *
+ * @link https://eslint.org/docs/developer-guide/nodejs-api
+ */
+( async function main() {
+	// 1. Create an instance with the `fix` option.
+	const eslint = new ESLint( {
+		fix: true,
+	} );
+
+	// 2. Lint files. This doesn't modify target files.
+	const results = await eslint.lintFiles( [
+		packageConfig.workingDirectory + '/src/**/*.{js,jsx,ts,tsx}',
+	] );
+
+	// 3. Modify the files with the fixed code.
+	await ESLint.outputFixes( results );
+
+	// 4. Format the results.
+	const formatter = await eslint.loadFormatter( 'stylish' );
+	const resultText = formatter.format( results );
+
+	// 5. Output it.
+	console.log( resultText );
+}() ).catch( error => {
+	process.exitCode = 1;
+	console.error( error );
+} );
