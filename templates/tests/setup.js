a3f3fdf5c69e1930c24aaa9bec3decfac70280dc
lipemat <mat@matlipe.com>
2022-04-06 16:14:52 -0400

    Include Enzyme adapter for React 17
    The `enzyme-adapter-react-16` used to be included by dependencies, now
    for React 17 we must use the unofficial
    `@wojtekmaj/enzyme-adapter-react-17` for Enzyme testing.

--- templates/tests/setup.js
+++ templates/tests/setup.js
@@ -1,8 +1,14 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import {configure} from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+require( 'unfetch/polyfill' ); // So we can use window.fetch.
+
+configure( {adapter: new Adapter()} );
 
 // Mock environmental variables
 global.__TEST__ = true;
 window.CORE_CONFIG = {
-	endpoint: 'http://starting-point.loc/wp-json/'
+	endpoint: 'http://starting-point.loc/wp-json/',
 };
