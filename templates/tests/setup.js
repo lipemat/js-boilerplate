3ed0a9bb5b24dd15f135e3a462bd25ec087d3993
lipemat <mat@matlipe.com>
2022-04-13 13:44:51 -0400

    Support cookies in JEST tests.

--- templates/tests/setup.js
+++ templates/tests/setup.js
@@ -10,6 +10,15 @@ configure( {adapter: new Adapter()} );
 // eslint-disable-next-line no-undef
 jest.spyOn( global.console, 'warn' ).mockImplementation( () => jest.fn() );
 
+// Support cookies.
+let __cookies = '';
+Object.defineProperty( window.document, 'cookie', {
+	get: () => __cookies,
+	set: v => __cookies = v,
+	split: s => __cookies.split( s ),
+} );
+
+
 // Mock environmental variables
 global.__TEST__ = true;
 window.CORE_CONFIG = {
