b34e2d34ea2045aa5846aaa40beea57393c691e4
lipemat <mat@matlipe.com>
2023-02-21 21:49:02 -0500

    Create a `.running` file within the /dist directory when "start" is running
    The file exists when Webpack is running and is automatically removed when
    Webpack stops.
    
    Allows to conditionally load scripts based on the state of Webpack Dev Server

--- scripts/start.js
+++ scripts/start.js
@@ -2,12 +2,28 @@ process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
 
 const webpack = require( 'webpack' );
+const config = require( '../helpers/package-config' );
+const {unlinkSync, writeFile} = require( 'fs' );
 const WebpackDevServer = require( 'webpack-dev-server' );
 const configHelper = require( '../helpers/config' );
+const path = require( 'path' );
 const webpackConfig = configHelper.getConfig( 'webpack.dev.js' );
 const devServerConfig = configHelper.getConfig( 'dev-server.config.js' );
 
 const server = new WebpackDevServer( devServerConfig, webpack( webpackConfig ) );
+
+/**
+ * Create a `.running` file within the `dist` which only
+ * exists if this script is running.
+ */
+writeFile( path.resolve( config.workingDirectory, 'dist/.running' ), Date.now().toString(), err => {
+	if ( err ) {
+		throw err;
+	}
+	process.on( 'exit', () => {
+		unlinkSync( path.resolve( config.workingDirectory, 'dist/.running' ) );
+	} );
+} );
 
 ( async() => {
 	try {
