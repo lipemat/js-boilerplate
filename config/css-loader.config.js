c68dce9f1cbb7edfa08a72b83b71be32252e703b
lipemat <mat@matlipe.com>
2023-03-09 14:56:01 -0500

    Fix :global CSS handling from 'pcss' and 'global' directories
    Previously when running Webpack Dev server it worked correctly, but not when
    dist ran.
    
    Split to a shared function for easy reuse.

--- config/css-loader.config.js
+++ config/css-loader.config.js
@@ -5,25 +5,30 @@ const {getLocalIdent} = require( '../helpers/css-classnames' );
  * Options for the Webpack `css-loader`.
  *
  * Extracted to its on file to allow easy overrides.
- *
- * @since 8.6.0
  */
+
+/**
+ * Default to :global for classes in "global" or "pcss" directories.
+ *
+ * @param {string} resourcePath
+ * @return {string}
+ */
+const mode = resourcePath => {
+	if ( /globals?\//i.test( resourcePath.replace( /\\/g, '/' ) ) ) {
+		return 'global';
+	}
+	if ( /pcss?\//i.test( resourcePath.replace( /\\/g, '/' ) ) ) {
+		return 'global';
+	}
+	return 'local';
+};
 
 let cssLoader = {
 	importLoaders: 1,
 	modules: {
 		exportLocalsConvention: 'camelCase',
 		localIdentName: 'Ⓜ[name]__[local]__[contenthash:base64:2]',
-		// Default to :global for classes in "global" or "pcss" directories.
-		mode: resourcePath => {
-			if ( /globals?\//i.test( resourcePath.replace( /\\/g, '/' ) ) ) {
-				return 'global';
-			}
-			if ( /pcss?\//i.test( resourcePath.replace( /\\/g, '/' ) ) ) {
-				return 'global';
-			}
-			return 'local';
-		},
+		mode,
 	},
 	sourceMap: true,
 	url: false,
@@ -38,10 +43,7 @@ if ( 'production' === process.env.NODE_ENV ) {
 			...config.shortCssClasses ? {getLocalIdent} : {},
 			// Hash used when short CSS classes are not enabled.
 			localIdentName: '[contenthash:base64:5]',
-			// Default to :global for classes in "global" directories.
-			mode: resourcePath => {
-				return /globals?\//i.test( resourcePath.replace( /\\/g, '/' ) ) ? 'global' : 'local';
-			},
+			mode,
 		},
 		url: false,
 	};
