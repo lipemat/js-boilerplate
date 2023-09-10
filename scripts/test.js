30285d88e2e26a498033c83bae498d775145aa0a
lipemat <mat@matlipe.com>
2023-09-10 11:11:14 -0500

    Support running tests out of the /jest directory
    Provide another level to allow organization.

--- scripts/test.js
+++ scripts/test.js
@@ -1,12 +1,27 @@
-/**
- * Runs `jest` with any arguments passed via the terminal or yarn.
- *
- * @notice you must add `/templates/jest.config.js` to the `jsPath` of your project.
- *
- * @example `lipemat-js-boilerplate test`
- */
+const {resolve: pathResolve} = require( 'path' );
+const {existsSync} = require( 'fs' );
 
-const packageConfig = require( '../helpers/package-config' );
-process.argv.push( '--config', packageConfig.workingDirectory + '/jest.config.js' );
+const {getPackageConfig} = require( '../helpers/package-config' );
+
+const packageConfig = getPackageConfig();
+
+const possibleConfig = [
+	// @todo Remove pulling from the root on next major release.
+	pathResolve( packageConfig.workingDirectory + '/jest.config.ts' ),
+	pathResolve( packageConfig.workingDirectory + '/jest.config.js' ),
+
+	// New location.
+	pathResolve( packageConfig.workingDirectory + '/jest/jest.config.ts' ),
+	pathResolve( packageConfig.workingDirectory + '/jest/jest.config.js' ),
+].filter( existsSync );
+
+if ( possibleConfig.length < 1 ) {
+	throw new Error( 'You must have a `jest.config.[tj]s` file in the root of your project or in the `jest` folder.' );
+} else {
+	const file = possibleConfig.shift();
+	if ( undefined !== file ) {
+		process.argv.push( '--config', file );
+	}
+}
 
 require( 'jest-cli/bin/jest' );
