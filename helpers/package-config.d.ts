34b749244aae1b0b1ebffd5f276589d880f122db
lipemat <mat@matlipe.com>
2023-10-24 13:12:36 -0500

    Introduce type definitions for the package-config helper
    Likely move to a .ts file later. For now this allows other TS files to use
    'package-config.js'
     as
    an import.

---
+++ helpers/package-config.d.ts
@@ -0,0 +1,43 @@
+export interface PackageConfig {
+	author?: string;
+	brotliFiles: boolean;
+	certificates?: Certificates;
+	combinedJson: boolean;
+	css_folder: string;
+	dependencies: Dependencies;
+	description?: string;
+	devDependencies: Dependencies;
+	es6Modules?: string[];
+	jsPath: string;
+	license?: string;
+	name?: string;
+	packageDirectory: string;
+	packageManager?: string;
+	resolutions?: Dependencies;
+	scripts: Partial<Scripts>;
+	shortCssClasses: boolean;
+	url: string;
+	version?: string;
+	workingDirectory: string;
+}
+
+export interface Dependencies {
+	[ name: string ]: string;
+}
+
+export interface Certificates {
+	cert: string;
+	key: string;
+}
+
+export interface Scripts {
+	browserslist: string;
+	dist: string;
+	lint: string;
+	postinstall: string;
+	start: string;
+	test: string;
+}
+
+declare const packageConfig: PackageConfig;
+export default packageConfig;
