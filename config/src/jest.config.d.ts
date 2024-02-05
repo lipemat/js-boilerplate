1adb502ae2ab7cdb5bf2ddab12ec03a209594d8d
lipemat <mat@matlipe.com>
2024-02-05 14:52:20 -0600

    Cleanup dangling files accidentally generated by tsc

--- config/src/jest.config.d.ts
+++
@@ -1,5 +0,0 @@
-import type {Config} from 'jest';
-
-export type JestConfig = Pick<Config, 'globals' | 'moduleNameMapper' | 'roots' | 'testEnvironment' | 'testEnvironmentOptions' | 'transform' | 'transformIgnorePatterns' | 'setupFilesAfterEnv'>;
-declare const jestConfig: Config;
-export default jestConfig;
